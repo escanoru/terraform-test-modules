acfec3c0fdffddf8075e4acf2941750ebe335004
Escanoru <44711170+escanoru@users.noreply.github.com>
2022-12-30 11:55:20 -0600

    Adding iam-services module
    * adding new changes
    
    * output updated
    
    * output updated
    
    * output updated
    
    * output updated
    
    * output updated
    
    * output updated

--- modules/landing-zone/iam-user/outputs.tf
+++ modules/landing-zone/iam-user/outputs.tf
@@ -2,3 +2,9 @@ output "user_arn" {
   value       = aws_iam_user.example.arn
   description = "The ARN of the created IAM user"
 }
+
+
+output "user_attributes" {
+  value       = aws_iam_user.example
+  description = "The ARN of the created IAM user"
+}
