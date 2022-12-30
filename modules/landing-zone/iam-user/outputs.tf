fdfc1d989370a7fcdf0cd4ea14625c72c588401c
escanoru <yitzoyi@outlook.com>
2022-12-30 16:20:01 +0000

    output updated

--- modules/landing-zone/iam-user/outputs.tf
+++ modules/landing-zone/iam-user/outputs.tf
@@ -1,4 +1,4 @@
-output "user_arn" {
-  value       = aws_iam_user.example.arn
-  description = "The ARN of the created IAM user"
+output "all_users" {
+  value       = aws_iam_user.example
+  description = "The ARNs of the created IAM users"
 }
