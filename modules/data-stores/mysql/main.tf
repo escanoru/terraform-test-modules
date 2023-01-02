e754c5d491fc9cb1ae71f8a041a58ae65e177476
escanoru <yitzoyi@outlook.com>
2023-01-02 03:36:45 +0000

    arguments oder updated

--- modules/data-stores/mysql/main.tf
+++ modules/data-stores/mysql/main.tf
@@ -4,8 +4,9 @@ provider "aws" {
 
 // Amazon RDS can take roughly 10 minutes to provision even a small database, so be patient
 resource "aws_db_instance" "example" { // aws_db_instance for AWS RDS
-  identifier_prefix   = var.rds_identifier_prefix
+
   engine              = "mysql"
+  identifier_prefix   = var.rds_identifier_prefix
   allocated_storage   = var.allocated_storage
   instance_class      = var.instance_class
   skip_final_snapshot = true // The final snapshot is disabled, as this code is just for learning and testing (if you don’t disable the snapshot, or don’t provide a name for the snapshot via the final_snapshot_identifier parameter, destroy will fail).
