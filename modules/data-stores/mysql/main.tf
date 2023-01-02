03dac48cee6a1c45c0b13ec2ac8b61ba0963fd1c
escanoru <yitzoyi@outlook.com>
2023-01-02 04:29:13 +0000

    tags attributes updated

--- modules/data-stores/mysql/main.tf
+++ modules/data-stores/mysql/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = var.aws_region
-  default_tags {
-    tags = var.db_tags 
-  }
+  # default_tags {
+  #   tags = var.db_tags 
+  # }
 }
 
 // Amazon RDS can take roughly 10 minutes to provision even a small database, so be patient
@@ -16,5 +16,5 @@ resource "aws_db_instance" "example" { // aws_db_instance for AWS RDS
   allocated_storage   = var.allocated_storage
   instance_class      = var.instance_class
   skip_final_snapshot = true // The final snapshot is disabled, as this code is just for learning and testing (if you don’t disable the snapshot, or don’t provide a name for the snapshot via the final_snapshot_identifier parameter, destroy will fail).
-  # tags                = var.db_tags
+  tags                = var.db_tags
 }
