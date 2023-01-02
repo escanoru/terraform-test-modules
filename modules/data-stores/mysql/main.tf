b5c9abc5e90070dc87490d9ec30e0f8b2309eee7
escanoru <yitzoyi@outlook.com>
2023-01-02 04:25:49 +0000

    arguments oder updated

--- modules/data-stores/mysql/main.tf
+++ modules/data-stores/mysql/main.tf
@@ -1,12 +1,8 @@
 provider "aws" {
   region = var.aws_region
-  # default_tags {
-  #   tags = {
-  #     Owner        = var.owner
-  #     Environment  = var.environment
-  #     ManagedBy    = "Terraform"
-  #   }
-  # }
+  default_tags {
+    tags = var.db_tags 
+  }
 }
 
 // Amazon RDS can take roughly 10 minutes to provision even a small database, so be patient
@@ -20,5 +16,5 @@ resource "aws_db_instance" "example" { // aws_db_instance for AWS RDS
   allocated_storage   = var.allocated_storage
   instance_class      = var.instance_class
   skip_final_snapshot = true // The final snapshot is disabled, as this code is just for learning and testing (if you don’t disable the snapshot, or don’t provide a name for the snapshot via the final_snapshot_identifier parameter, destroy will fail).
-  tags                = var.db_tags     
+  # tags                = var.db_tags
 }
