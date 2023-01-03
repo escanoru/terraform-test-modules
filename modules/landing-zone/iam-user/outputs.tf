040af7d3019d525dd0990d3ee942ec3aac9e7be5
escanoru <yitzoyi@outlook.com>
2023-01-03 03:36:36 +0000

    count logic added

--- modules/landing-zone/iam-user/outputs.tf
+++ modules/landing-zone/iam-user/outputs.tf
@@ -3,8 +3,15 @@ output "user_arn" {
   description = "The ARN of the created IAM user"
 }
 
-
 output "user_attributes" {
   value       = aws_iam_user.example
   description = "The ARN of the created IAM user"
+}
+
+output "neo_cloudwatch_policy_arn" {
+  value = (
+    var.give_neo_cloudwatch_full_access
+    ? aws_iam_user_policy_attachment.neo_cloudwatch_full_access[0].policy_arn
+    : aws_iam_user_policy_attachment.neo_cloudwatch_read_only[0].policy_arn
+  )
 }
