b3f3dbd7b2e6c6e893c1c354d7ca5cb3b4f9f31d
escanoru <yitzoyi@outlook.com>
2023-01-04 22:37:08 +0000

    removed duplicates attributes

--- modules/data-stores/mysql/main.tf
+++ modules/data-stores/mysql/main.tf
@@ -13,9 +13,6 @@ terraform {
 
 // Amazon RDS can take roughly 10 minutes to provision even a small database, so be patient
 resource "aws_db_instance" "example" { // aws_db_instance for AWS RDS
-  identifier_prefix   = var.rds_identifier_prefix
-  allocated_storage   = var.allocated_storage
-  instance_class      = var.instance_class
   skip_final_snapshot = true // // If you don’t skip (disable) the snapshot, or don’t provide a name for the snapshot via the final_snapshot_identifier parameter, destroy will fail).
   tags                = var.db_tags
 
@@ -31,7 +28,7 @@ resource "aws_db_instance" "example" { // aws_db_instance for AWS RDS
   username               = var.replicate_source_db == null ? var.db_username : null // export TF_VAR_db_username="<YOUR_DB_USERNAME>"
   password               = var.replicate_source_db == null ? var.db_password : null // export TF_VAR_db_password="<YOUR_DB_PASSWORD>"
   instance_class         = var.replicate_source_db == null ? var.instance_class : null
-  rds_identifier_prefix  = var.replicate_source_db == null ? var.rds_identifier_prefix : null
+  identifier_prefix  = var.replicate_source_db == null ? var.rds_identifier_prefix : null
   allocated_storage      = var.replicate_source_db == null ? var.allocated_storage : null
 
 }
