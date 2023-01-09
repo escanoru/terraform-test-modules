811cfa98c0091220f2c827a19ae8004f59f67333
escanoru <yitzoyi@outlook.com>
2023-01-09 02:30:59 +0000

    removed unused variable

--- modules/networking/alb/main.tf
+++ modules/networking/alb/main.tf
@@ -26,7 +26,7 @@ resource "aws_lb" "example" {
   name               = var.alb_name
   load_balancer_type = "application"
   subnets            = var.subnet_ids
-  security_groups    = [aws_security_group.alb_sg.id]
+  security_groups    = [aws_security_group.alb.id]
 }
 
 // Now we need to define the listener, the target group and the listerner rules, each has its own terraform resource:
