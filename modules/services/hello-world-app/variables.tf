701cbe115182648077711fe99aece5bb2d1a3dc8
escanoru <yitzoyi@outlook.com>
2023-01-09 02:25:28 +0000

    removed unused variable

--- modules/services/hello-world-app/variables.tf
+++ modules/services/hello-world-app/variables.tf
@@ -1,8 +1,3 @@
-variable "cluster_name" {
-  description = "The name to use for all the cluster resources"
-  type        = string
-}
-
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
